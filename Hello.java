c838ca1881e89b780b52c5d57f05f3600114817e
Becky <rebecca.loveking@gmail.com>
2016-04-05 10:16:21 -0400

    Hello

--- Hello.java
+++ Hello.java
@@ -1,6 +1,10 @@
 package com.naveen;
 class Hello{
 public void sayHi(){
-System.out.println("hi");
+System.out.println("hi from get to Jenkins.");
+}
+
+public static void main (String args[]){
+new Hello().sayHi();
 }
 }
