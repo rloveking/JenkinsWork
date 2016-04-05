e8cfc3bf56fb2e2fec3a5bdb6f40e45a042b8df7
Becky <rebecca.loveking@gmail.com>
2016-04-05 10:20:22 -0400

    Hello

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 package com.naveen;
 class Hello{
 public void sayHi(){
-System.out.println("hi from get to Jenkins.");
+System.out.println("hi from git to Jenkins.");
 }
 
 public static void main (String args[]){
